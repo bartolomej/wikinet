18e6b9fbc6317946fb7ccb512fda9e6cc80a6c80
bart <bartolomej.kozorog@gmail.com>
2019-06-27 17:47:47 +0200

    Added simple index page with form

--- routes/views.js
+++ routes/views.js
@@ -1,13 +1,23 @@
 const express = require('express');
 const router = express.Router();
 
+router.get('/', (req, res, next) => {
+  res.render('index');
+});
 
 router.get('/demo', (req, res, next) => {
   res.render('demo', { title: 'Express' });
 });
 
 router.get('/graph', (req, res, next) => {
-  res.render('graph', { title: 'Express' });
+  let renderTime = req.query.render_time;
+  let link = req.query.link;
+  let limit = req.query.limit;
+  res.render('graph', {
+    renderTime,
+    link,
+    limit
+  });
 });
 
 router.get('/node', (req, res, next) => {
